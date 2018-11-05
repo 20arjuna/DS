2d437028f2f8d60e5e73a9963813843354e30380
Arjun Akkiraju <20arjuna@students.harker.org>
2018-11-05 10:26:41 -0800

    created class skeleton for Card class

---
+++ Solitaire/Card.java
@@ -0,0 +1,39 @@
+
+public class Card
+{
+    private int rank;
+    private String suit;
+    private boolean isFaceUp;
+    
+    public Card(int r, String s)
+    {
+        rank = r;
+        s = suit;
+        isFaceUp = false;
+    }
+    public int getRank()
+    {
+        
+    }
+    public String getSuit()
+    {
+        
+    }
+    public boolean isRed()
+    {
+        
+    }
+    public boolean isFaceUp()
+    {
+        
+    }
+    public void turnUp()
+    {
+        
+    }
+    public void turnDown()
+    {
+        
+    }
+    
+}
