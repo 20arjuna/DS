30f87afb8f4f1597bdd9441d53a005dcb6b803dc
Arjun Akkiraju <20arjuna@students.harker.org>
2019-01-21 14:57:56 -0800

    Finished Coding Heas lab

---
+++ Heaps/src/HeapUtils.java
@@ -0,0 +1,73 @@
+
+public class HeapUtils
+{
+    public static void heapify(Comparable[] heap, int index, int heapSize)
+    {
+        int left = (int) heap[1];
+        int right = (int) heap[2];
+        int largest =0;
+        if(left < heapSize)
+        {
+            if(left > (int)heap[0])
+            {
+                largest = left;
+            }
+        }
+        if(right < heapSize)
+        {
+            if(right > (int)heap[0])
+            {
+                largest = Math.max(right, left);
+            }
+        }
+        if(largest != index)
+        {
+            heap = swap(heap, largest, index);
+        }
+        heapify(heap, largest, heapSize);
+    }
+    private static Comparable [] swap(Comparable[] heap, int i1,int i2)
+    {
+        Comparable temp = heap[i1];
+        heap[i1] = heap[i2];
+        heap[i2] = temp;
+        return heap;
+    }
+    public static void buildHeap(Comparable[] heap, int heapSize)
+    {
+        for(int i = heap.length/2; i >= 1; i--)
+        {
+            heapify(heap, i, (int)heap[i]-(int)heap[1]);
+        }
+    }
+    public static Comparable[] insert(
+            Comparable[] heap, Comparable item, int heapSize)
+    {
+        Comparable[] newHeap = new Comparable[heap.length+1];
+        for(int i = 0; i< heap.length; i++)
+        {
+            newHeap[i]=heap[i];
+        }
+        newHeap[newHeap.length -1] = item;
+        return newHeap;
+    }
+    public static Comparable remove(Comparable[] heap, int heapSize)
+    {
+        Comparable temp = heap[1];
+        heap[1] = null;
+        swap(heap, (int)heap[heapSize], (int)heap[1]);
+        return temp;
+        
+    }
+    public static Comparable[] heapSort(Comparable[] heap, int heapSize)
+    {
+       for(int i = heapSize-1; i>=0; i--)
+       {
+           buildHeap(heap, heapSize);
+           heapify(heap, i, heapSize);
+           remove(heap, heapSize);
+       }
+       return heap;
+        
+    }
+}
---
+++ Heaps/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+       Integer [] intObjs = new Integer [12];
+       for(int i =1; i < intObjs.length; i++)
+       {
+           intObjs[i] = (int) ((Math.random() * 99) + 1);
+       }
+       HeapDisplay display = new HeapDisplay();
+       display.displayHeap(intObjs, 11);
+       for(int x = 0; x < intObjs.length; x++)
+       {
+           System.out.println(intObjs[x]);
+       }
+    }
+
+}
