f3750f15a5c5f261f001731a38828f29d23dab8e
Arjun Akkiraju <20arjuna@students.harker.org>
2019-01-21 11:19:44 -0800

    Binary Trees Lab Submitted!

--- BinaryTrees/src/TreeDriver.java
+++ BinaryTrees/src/TreeDriver.java
@@ -1,45 +1,25 @@
 import java.util.*;
+/**
+ * TreeDriver is the driver class for the Binary Trees Lab.
+ * It is used to run and test all TreeUtil Methods.
+ * @author Arjun Akkiraju
+ * @version 1.21.19
+ *
+ */
 public class TreeDriver
 {
-
+    /**
+     * Main Method for running TreeUtil Methods
+     * @param args Arguments from the command line. 
+     */
     public static void main(String[] args)
     {
-        //TreeNode tree = TreeUtil.createRandom(4);
+        
         TreeNode tree = TreeUtil.loadTree("test.txt");
         TreeDisplay display = new TreeDisplay();
         display.displayTree(tree);
         List<String> list = new ArrayList<String>();
-        /*
-        System.out.println("Leftmost: " + TreeUtil.leftmost(tree));
-        System.out.println("Rightmost: " + TreeUtil.rightmost(tree));
-        System.out.println("Max depth: " + TreeUtil.maxDepth(tree));
-        System.out.println("Number of nodes: " + TreeUtil.countNodes(tree));
-        System.out.println("Number of leaves: " + TreeUtil.countLeaves(tree));*/
         
-       /* Scanner sc = new Scanner(System.in);
-        System.out.println("1. PreOrder");
-        System.out.println("2. InOrder");
-        System.out.println("3. PostOrder");
-        
-        int choice = sc.nextInt();
-        if(choice == 1) 
-        {
-            TreeUtil.preOrder(tree, display);
-        }
-        else if(choice==2)
-        {
-            TreeUtil.inOrder(tree, display);
-        }
-        else if(choice ==3)
-        {
-            TreeUtil.postOrder(tree, display);
-        }*/
-        //TreeUtil.fillList(tree, list);
-        //System.out.println(list);
-        
-        //TreeUtil.saveTree("test.txt", tree);
-        
-
     }
 
 }
